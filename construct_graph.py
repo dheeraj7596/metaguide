8f9e3d64fdfea1723676218ae13c3818ebe4d6ed
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-02-02 15:21:47 -0800

    Graph constructed.

--- construct_graph.py
+++ construct_graph.py
@@ -1,27 +1,74 @@
 import pickle
-import itertools
+from scipy import sparse
+import numpy as np
+
+
+def make_authors_map(df):
+    count = len(df)
+    author_id = {}
+    id_author = {}
+
+    authors_set = set()
+    for auts in df.authors:
+        authors_set.update(set(auts.split(",")))
+
+    for i, aut in enumerate(authors_set):
+        author_id[aut] = count
+        id_author[count] = aut
+        count += 1
+    return author_id, id_author, count
+
+
+def make_venues_map(df):
+    count = len(df)
+    venues_set = set(df.conf)
+    venue_id = {}
+    id_venue = {}
+
+    for i, venue in enumerate(venues_set):
+        venue_id[venue] = count
+        id_venue[count] = venue
+        count += 1
+
+    return venue_id, id_venue, count
+
 
 if __name__ == "__main__":
-    base_path = "/data4/dheeraj/metaguide/"
-    dataset = "arxiv_cs/"
+    base_path = "./data/"
+    dataset = "dblp/"
 
     data_path = base_path + dataset
-    df = pickle.load(open(data_path + "df_cs_2014_filtered.pkl", "rb"))
-    label_id = pickle.load(open(data_path + "label_id.pkl", "rb"))
-    id_label = pickle.load(open(data_path + "id_label.pkl", "rb"))
-    author_id = pickle.load(open(data_path + "author_id.pkl", "rb"))
-    id_author = pickle.load(open(data_path + "id_author.pkl", "rb"))
+    df = pickle.load(open(data_path + "df_mapped_labels_phrase_removed_stopwords.pkl", "rb"))
 
-    author_pairs = []
+    author_id, id_author, auth_graph_node_count = make_authors_map(df)
+    venue_id, id_venue, venue_graph_node_count = make_venues_map(df)
 
+    edges = []
+    weights = []
     for i, row in df.iterrows():
-        authors = row["authors"]
-        ids = [author_id[aut] for aut in authors]
-        assert len(ids) > 0
-        if len(ids) >= 2:
-            author_pairs.append(list(itertools.combinations(ids, 2)))
-        else:
-            author_pairs.append([(ids[0],)])
+        auth_str = row["authors"]
+        authors = auth_str.split(",")
+        for auth in authors:
+            edges.append([i, author_id[auth]])
+            weights.append(1)
+    edges = np.array(edges)
+    G_auth = sparse.csr_matrix((weights, (edges[:, 0], edges[:, 1])),
+                               shape=(auth_graph_node_count, auth_graph_node_count))
 
-    df["author pairs"] = author_pairs
-    pickle.dump(df, open(data_path + "df_cs_2014_filtered_authorpairs.pkl", "wb"))
+    edges = []
+    weights = []
+    for i, row in df.iterrows():
+        conf = row["conf"]
+        edges.append([i, venue_id[conf]])
+        weights.append(1)
+    edges = np.array(edges)
+    G_conf = sparse.csr_matrix((weights, (edges[:, 0], edges[:, 1])),
+                               shape=(venue_graph_node_count, venue_graph_node_count))
+
+    sparse.save_npz(data_path + "G_auth.npz", G_auth)
+    sparse.save_npz(data_path + "G_conf.npz", G_conf)
+    pickle.dump(venue_id, open(data_path + "venue_id.pkl", "wb"))
+    pickle.dump(id_venue, open(data_path + "id_venue.pkl", "wb"))
+
+    pickle.dump(author_id, open(data_path + "author_id.pkl", "wb"))
+    pickle.dump(id_author, open(data_path + "id_author.pkl", "wb"))

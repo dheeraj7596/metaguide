620b535b1eeb681a42549cd07407e5124a392799
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-02-18 16:20:56 -0800

    Added author part in the selection

--- cocube_beta.py
+++ cocube_beta.py
@@ -123,6 +123,25 @@ def update_label_conf_dict(label_conf_dict, it):
     return label_conf_dict
 
 
+def update_by_percent_with_overlap(label_entity_dict, entity_docid_map, df, i):
+    filtered_dict = {}
+    for l in label_entity_dict:
+        filtered_dict[l] = {}
+
+    n = min((i + 1) * 0.1 * len(df), len(df) * 0.6)
+    doc_id_set = set()
+    i = 0
+    while len(doc_id_set) < n:
+        for l in label_entity_dict:
+            all_tups = list(label_entity_dict[l].items())
+            if i < len(all_tups):
+                tup = all_tups[i]
+                filtered_dict[l][tup[0]] = tup[1]
+                doc_id_set.update(entity_docid_map[tup[0]])
+        i += 1
+    return filtered_dict
+
+
 def update_by_percent(label_phrase_dict, phrase_docid_map, df, i):
     filtered_dict = {}
     for l in label_phrase_dict:
@@ -185,6 +204,7 @@ if __name__ == "__main__":
     fnust_id = pickle.load(open(pkl_dump_dir + "fnust_id.pkl", "rb"))
     id_fnust = pickle.load(open(pkl_dump_dir + "id_fnust.pkl", "rb"))
     phrase_docid_map = pickle.load(open(pkl_dump_dir + "phrase_docid_map.pkl", "rb"))
+    author_docid_map = pickle.load(open(pkl_dump_dir + "author_docid_map.pkl", "rb"))
 
     label_phrase_dict = modify(label_term_dict)
     label_author_dict = {}
@@ -216,12 +236,13 @@ if __name__ == "__main__":
 
         label_phrase_dict = run_pagerank(probs, df, G_phrase, fnust_id, id_fnust, label_to_index, phrase_plot_dump_dir,
                                          plot=plot)
-        # label_author_dict = run_pagerank(probs, df, G_auth, author_id, id_author, label_to_index, auth_plot_dump_dir,
-        #                                  plot=plot)
+        label_author_dict = run_pagerank(probs, df, G_auth, author_id, id_author, label_to_index, auth_plot_dump_dir,
+                                         plot=plot)
         # label_conf_dict = run_pagerank(probs, df, G_conf, venue_id, id_venue, label_to_index, conf_plot_dump_dir,
         #                                plot=plot)
 
         label_phrase_dict = update_by_percent(label_phrase_dict, phrase_docid_map, df, i)
+        label_author_dict = update_by_percent_with_overlap(label_author_dict, author_docid_map, df, i)
         # label_phrase_dict = update_label_entity_dict_with_iteration(label_phrase_dict, df, pred_labels, i)
         # label_author_dict = update_label_entity_dict_with_iteration(label_author_dict, df, pred_labels, i)
         # label_conf_dict = update_label_conf_dict(label_conf_dict, i)
--- test.py
+++ test.py
@@ -1,5 +1,41 @@
 from word2vec_orig import create_corpus, create_vocabulary
+from construct_graph import detect_phrase
 import pickle
+
+
+def create_phrase_doc_id_map(df, tokenizer, phrase_id_map):
+    phrase_docid = {}
+
+    index_word = {}
+    for w in tokenizer.word_index:
+        index_word[tokenizer.word_index[w]] = w
+
+    id_phrase_map = {}
+    for ph in phrase_id_map:
+        id_phrase_map[phrase_id_map[ph]] = ph
+
+    for i, row in df.iterrows():
+        abstract_str = row["abstract"]
+        phrases = detect_phrase(abstract_str, tokenizer, index_word, id_phrase_map, i)
+        for ph in phrases:
+            try:
+                phrase_docid[ph].add(i)
+            except:
+                phrase_docid[ph] = {i}
+    return phrase_docid
+
+
+def create_author_doc_id_map(df):
+    author_docid = {}
+    for i, row in df.iterrows():
+        auts = row["authors"].strip().split(",")
+        for author in auts:
+            try:
+                author_docid[author].add(i)
+            except:
+                author_docid[author] = {i}
+    return author_docid
+
 
 if __name__ == "__main__":
     base_path = "./data/"
@@ -7,6 +43,10 @@ if __name__ == "__main__":
     data_path = base_path + dataset
 
     df = pickle.load(open(data_path + "df_mapped_labels_phrase_removed_stopwords_test.pkl", "rb"))
-    corpus = create_corpus(df)
-    vocabulary, vocab_to_int, int_to_vocab, tokenizer = create_vocabulary(corpus, num_words=50000)
-    pickle.dump(tokenizer, open(data_path + "tokenizer_test.pkl", "wb"))
+    # tokenizer = pickle.load(open(data_path + "tokenizer.pkl", "rb"))
+    # phrase_id_map = pickle.load(open(data_path + "phrase_id_map.pkl", "rb"))
+
+    # phrase_docid = create_phrase_doc_id_map(df, tokenizer, phrase_id_map)
+    author_docid_map = create_author_doc_id_map(df)
+
+    pickle.dump(author_docid_map, open(data_path + "author_docid_map.pkl", "wb"))

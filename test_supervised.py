6a4b1081462d5fc4cc30c13c680394b9494f4d0c
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-03-14 23:52:57 -0700

    supervised code changed for yelp

--- test_supervised.py
+++ test_supervised.py
@@ -12,33 +12,33 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 def run(df):
-    # glove_dir = basepath + "glove.6B"
+    glove_dir = basepath + "glove.6B"
     model_name = "cocube_tok"
     max_sentence_length = 100
     max_sentences = 15
     max_words = 20000
     embedding_dim = 100
 
-    X = df["abstract"]
+    X = df["Review"]
     y = df["label"]
     y_true = df["label"]
 
     labels, label_to_index, index_to_label = get_distinct_labels(df)
     y_one_hot = make_one_hot(y, label_to_index)
     # y = np.array(y)
-    # print("Fitting tokenizer...")
-    # tokenizer = fit_get_tokenizer(X, max_words)
-    print("Getting tokenizer")
-    tokenizer = pickle.load(open(basepath + dataset + "tokenizer.pkl", "rb"))
+    print("Fitting tokenizer...")
+    tokenizer = fit_get_tokenizer(X, max_words)
+    # print("Getting tokenizer")
+    # tokenizer = pickle.load(open(basepath + dataset + "tokenizer.pkl", "rb"))
     print("Splitting into train, dev...")
     X_train, y_train, X_val, y_val, X_test, y_test = create_train_dev_test(X, labels=y_one_hot, tokenizer=tokenizer,
                                                                            max_sentences=max_sentences,
                                                                            max_sentence_length=max_sentence_length,
                                                                            max_words=max_words)
-    # print("Creating Embedding matrix...")
-    # embedding_matrix = create_embedding_matrix(glove_dir, tokenizer, embedding_dim)
-    print("Getting Embedding matrix...")
-    embedding_matrix = pickle.load(open(basepath + dataset + "embedding_matrix.pkl", "rb"))
+    print("Creating Embedding matrix...")
+    embedding_matrix = create_embedding_matrix(glove_dir, tokenizer, embedding_dim)
+    # print("Getting Embedding matrix...")
+    # embedding_matrix = pickle.load(open(basepath + dataset + "embedding_matrix.pkl", "rb"))
     print("Initializing model...")
     model = HAN(max_words=max_sentence_length, max_sentences=max_sentences, output_size=len(y_train[0]),
                 embedding_matrix=embedding_matrix)
@@ -57,11 +57,11 @@ def run(df):
 
 if __name__ == "__main__":
     basepath = "/data4/dheeraj/metaguide/"
-    dataset = "dblp/"
+    dataset = "yelp/"
     pkl_dump_dir = basepath + dataset
 
     # df = pickle.load(open(pkl_dump_dir + "df_mapped_labels_phrase_removed_stopwords_test.pkl", "rb"))
-    df = pickle.load(open(pkl_dump_dir + "df_mapped_labels_phrase.pkl", "rb"))
+    df = pickle.load(open(pkl_dump_dir + "business_reviews.pkl", "rb"))
     print("*" * 80)
     print("RUNNING DBLP")
     run(df)

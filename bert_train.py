fd267b02c1cef6a5f67841d5c02053525c5de0f0
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-08-08 01:40:10 -0700

    num_labels fixed.

--- bert_train.py
+++ bert_train.py
@@ -89,12 +89,12 @@ def create_data_loaders(dataset):
     return train_dataloader, validation_dataloader
 
 
-def train(train_dataloader, validation_dataloader, device):
+def train(train_dataloader, validation_dataloader, device, num_labels):
     # Load BertForSequenceClassification, the pretrained BERT model with a single
     # linear classification layer on top.
     model = BertForSequenceClassification.from_pretrained(
         "bert-base-uncased",  # Use the 12-layer BERT model, with an uncased vocab.
-        num_labels=2,  # The number of output labels--2 for binary classification.
+        num_labels=num_labels,  # The number of output labels--2 for binary classification.
         # You can increase this for multi-class tasks.
         output_attentions=False,  # Whether the model returns attentions weights.
         output_hidden_states=False,  # Whether the model returns all hidden-states.
@@ -398,5 +398,5 @@ def train_bert(X, y, use_gpu=False):
     else:
         device = torch.device("cpu")
 
-    model = train(train_dataloader, validation_dataloader, device)
+    model = train(train_dataloader, validation_dataloader, device, num_labels=len(set(y)))
     return model
